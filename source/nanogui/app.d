2901d0aaf74c17ad03793daec3ce339eb0b747a7
drug007 <drug2004@bk.ru>
2018-04-22 01:35:49 +0300

    ArsdBackend added.

--- source/nanogui/app.d
+++ source/nanogui/app.d
@@ -2,57 +2,38 @@ import std.datetime : Clock;
 
 import arsd.simpledisplay;
 import arsd.nanovega;
+import nanogui.arsdbackend : ArsdBackend;
 
 void main () {
-	NVGContext nvg; // our NanoVega context
-	// we need at least OpenGL3 with GLSL to use NanoVega,
-	// so let's tell simpledisplay about that
-	setOpenGLContextVersion(3, 0);
-	// now create OpenGL window
-	auto sdmain = new SimpleWindow(1000, 800, "NanoVega Simple Sample", OpenGlOptions.yes, Resizability.allowResizing);
-	// we need to destroy NanoVega context on window close
-	// stricly speaking, it is not necessary, as nothing fatal
-	// will happen if you'll forget it, but let's be polite.
-	// note that we cannot do that *after* our window was closed,
-	// as we need alive OpenGL context to do proper cleanup.
-	sdmain.onClosing = delegate () {
-		nvg.kill;
-	};
+	
+	auto backend = new ArsdBackend();
 
 	import nanogui.screen : Screen;
 	import nanogui.widget, nanogui.theme, nanogui.checkbox, nanogui.label, 
 		nanogui.common, nanogui.window, nanogui.layout, nanogui.button,
 		nanogui.popupbutton, nanogui.entypo, nanogui.popup, nanogui.vscrollpanel;
 
-	Screen screen;
-
 	// this is called just before our window will be shown for the first time.
 	// we must create NanoVega context here, as it needs to initialize
 	// internal OpenGL subsystem with valid OpenGL context.
-	sdmain.visibleForTheFirstTime = delegate () {
-		// yes, that's all
-		nvg = nvgCreateContext();
-		assert(nvg !is null, "cannot initialize NanoGui");
-
-		screen = new Screen(sdmain.width, sdmain.height, Clock.currTime.stdTime);
-		screen.theme = new Theme(nvg);
-
+	backend.visibleForTheFirstTime = ()
+	{
 		{
-			auto window = new Window(screen, "Button demo");
+			auto window = new Window(backend.screen, "Button demo");
 			window.position(Vector2i(15, 15));
-			window.size = Vector2i(screen.size.x - 30, screen.size.y - 30);
+			window.size = Vector2i(backend.screen.size.x - 30, backend.screen.size.y - 30);
 			window.layout(new GroupLayout());
 
 			new Label(window, "Push buttons", "sans-bold");
 
-			auto checkbox = new CheckBox(window, "Checkbox #1", (bool value){ sdmain.redrawOpenGlSceneNow(); });
+			auto checkbox = new CheckBox(window, "Checkbox #1", (bool value){ backend.simple_window.redrawOpenGlSceneNow(); });
 			checkbox.position = Vector2i(100, 190);
-			checkbox.size = checkbox.preferredSize(nvg);
+			checkbox.size = checkbox.preferredSize(backend.nvg);
 			checkbox.checked = true;
 
 			auto label = new Label(window, "Label");
 			label.position = Vector2i(100, 300);
-			label.size = label.preferredSize(nvg);
+			label.size = label.preferredSize(backend.nvg);
 
 			Popup popup;
 
@@ -65,15 +46,15 @@ void main () {
 
 			auto popupBtn = new PopupButton(window, "PopupButton", Entypo.ICON_EXPORT);
 			popup = popupBtn.popup;
-	        popup.layout(new GroupLayout());
-	        new Label(popup, "Arbitrary widgets can be placed here");
-	        new CheckBox(popup, "A check box", null);
+			popup.layout(new GroupLayout());
+			new Label(popup, "Arbitrary widgets can be placed here");
+			new CheckBox(popup, "A check box", null);
 
-	        window.tooltip = "Button demo tooltip";
+			window.tooltip = "Button demo tooltip";
 		}
 
 		{
-			auto window = new Window(screen, "Button group example");
+			auto window = new Window(backend.screen, "Button group example");
 			window.position(Vector2i(200, 15));
 			window.layout(new GroupLayout());
 
@@ -97,29 +78,29 @@ void main () {
 			btn.tooltip = "Radio button THREE";
 			buttonGroup ~= btn;
 
-	        window.tooltip = "Radio button group tooltip";
+			window.tooltip = "Radio button group tooltip";
 		}
 
 		{
-			auto window = new Window(screen, "Yet another window");
+			auto window = new Window(backend.screen, "Yet another window");
 			window.position(Vector2i(300, 15));
 			window.layout(new GroupLayout());
 
 			new Label(window, "Message dialog", "sans-bold");
 			new CheckBox(window, "Checkbox #3", (bool value){ });
 
-	        window.tooltip = "Window with checkbox #3 tooltip";
+			window.tooltip = "Window with checkbox #3 tooltip";
 		}
 
 		{
-			auto window = new Window(screen, "Yet another window");
+			auto window = new Window(backend.screen, "Yet another window");
 			window.position(Vector2i(400, 15));
 			window.layout(new GroupLayout());
 
 			new Label(window, "Message dialog", "sans-bold");
 			new CheckBox(window, "Checkbox #4", (bool value){ });
 
-	        window.tooltip = "Window with checkbox FOUR tooltip";
+			window.tooltip = "Window with checkbox FOUR tooltip";
 		}
 
 		{
@@ -127,7 +108,7 @@ void main () {
 			int half_width = width / 2;
 			int height     = 200;
 
-			auto window = new Window(screen, "All Icons");
+			auto window = new Window(backend.screen, "All Icons");
 			window.position(Vector2i(0, 400));
 			window.fixedSize(Vector2i(width, height));
 
@@ -148,93 +129,10 @@ void main () {
 				item.fixedWidth(half_width);
 			}
 		}
-
+		
 		// now we should do layout manually yet
-		screen.performLayout(nvg);
+		backend.screen.performLayout(backend.nvg);
 	};
-
-	// this callback will be called when we will need to repaint our window
-	sdmain.redrawOpenGlScene = () {
-		screen.size = Vector2i(sdmain.width, sdmain.height);
-		screen.draw(nvg);
-	};
-	sdmain.eventLoop(40,
-		() {
-			// unfortunately screen may be not initialized
-			if (screen)
-			{
-				screen.currTime = Clock.currTime.stdTime;
-				if (screen.needToDraw)
-					sdmain.redrawOpenGlSceneNow();
-			}
-		},
-		delegate (KeyEvent event)
-		{
-			if (event == "*-Q" || event == "Escape") { sdmain.close(); return; } // quit on Q, Ctrl+Q, and so on
-		},
-		delegate (MouseEvent event)
-		{
-			import std.datetime : Clock;
-			import nanogui.common : MouseButton, MouseAction;
-
-			MouseButton btn;
-			MouseAction action;
-			int modifiers;
-
-			// convert event data from arsd.simpledisplay format
-			// to own format
-			switch(event.button)
-			{
-				case arsd.simpledisplay.MouseButton.left:
-					btn = MouseButton.Left;
-				break;
-				case arsd.simpledisplay.MouseButton.right:
-					btn = MouseButton.Right;
-				break;
-				case arsd.simpledisplay.MouseButton.middle:
-					btn = MouseButton.Middle;
-				break;
-				case arsd.simpledisplay.MouseButton.wheelUp:
-					btn = MouseButton.WheelUp;
-					screen.scrollCallbackEvent(0, -1, Clock.currTime.stdTime);
-				break;
-				case arsd.simpledisplay.MouseButton.wheelDown:
-					btn = MouseButton.WheelDown;
-					screen.scrollCallbackEvent(0, 1, Clock.currTime.stdTime);
-				break;
-				default:
-					btn = MouseButton.None;
-			}
-
-			final switch(event.type)
-			{
-				case arsd.simpledisplay.MouseEventType.buttonPressed:
-					action = MouseAction.Press;
-				break;
-				case arsd.simpledisplay.MouseEventType.buttonReleased:
-					action = MouseAction.Release;
-				break;
-				case arsd.simpledisplay.MouseEventType.motion:
-					action = MouseAction.Motion;
-					screen.cursorPosCallbackEvent(event.x, event.y, Clock.currTime.stdTime);
-				return;
-			}
-
-			if (event.modifierState & ModifierState.leftButtonDown)
-				modifiers |= MouseButton.Left;
-			if (event.modifierState & ModifierState.rightButtonDown)
-				modifiers |= MouseButton.Right;
-			if (event.modifierState & ModifierState.middleButtonDown)
-				modifiers |= MouseButton.Middle;
-
-			// propagating button events
-			if (event.type == MouseEventType.buttonPressed  ||
-				event.type == MouseEventType.buttonReleased ||
-				event.type == MouseEventType.motion)
-			{
-				screen.mouseButtonCallbackEvent(btn, action, modifiers, Clock.currTime.stdTime);
-			}
-		},
-	);
-	flushGui(); // let OS do it's cleanup
+	
+	backend.run();
 }
---
+++ source/nanogui/arsdbackend.d
@@ -0,0 +1,145 @@
+///
+module nanogui.arsdbackend;
+
+import std.datetime : Clock;
+import std.exception : enforce;
+
+import arsd.simpledisplay;
+import arsd.nanovega;
+
+import nanogui.screen : Screen;
+import nanogui.theme : Theme;
+import nanogui.common : Vector2i;
+
+class ArsdBackend
+{
+	this()
+	{
+		// we need at least OpenGL3 with GLSL to use NanoVega,
+		// so let's tell simpledisplay about that
+		setOpenGLContextVersion(3, 0);
+
+		simple_window = new SimpleWindow(1000, 800, "NanoVega Simple Sample", OpenGlOptions.yes, Resizability.allowResizing);
+		
+		// we need to destroy NanoVega context on window close
+		// stricly speaking, it is not necessary, as nothing fatal
+		// will happen if you'll forget it, but let's be polite.
+		// note that we cannot do that *after* our window was closed,
+		// as we need alive OpenGL context to do proper cleanup.
+		simple_window.onClosing = delegate () {
+			nvg.kill;
+		};
+
+		simple_window.visibleForTheFirstTime = () {
+			nvg = nvgCreateContext();
+			enforce(nvg !is null, "cannot initialize NanoGui");
+
+			screen = new Screen(simple_window.width, simple_window.height, Clock.currTime.stdTime);
+			screen.theme = new Theme(nvg);
+
+			// this callback will be called when we will need to repaint our window
+			simple_window.redrawOpenGlScene = () {
+				screen.size = Vector2i(simple_window.width, simple_window.height);
+				screen.draw(nvg);
+			};
+
+			if (user_dg !is null)
+				user_dg();
+		};
+	}
+
+	void run()
+	{
+		simple_window.eventLoop(40,
+			() {
+				// unfortunately screen may be not initialized
+				if (screen)
+				{
+					screen.currTime = Clock.currTime.stdTime;
+					if (screen.needToDraw)
+						simple_window.redrawOpenGlSceneNow();
+				}
+			},
+			delegate (KeyEvent event)
+			{
+				if (event == "*-Q" || event == "Escape") { simple_window.close(); return; } // quit on Q, Ctrl+Q, and so on
+			},
+			delegate (MouseEvent event)
+			{
+				import std.datetime : Clock;
+				import nanogui.common : MouseButton, MouseAction;
+
+				MouseButton btn;
+				MouseAction action;
+				int modifiers;
+
+				// convert event data from arsd.simpledisplay format
+				// to own format
+				switch(event.button)
+				{
+					case arsd.simpledisplay.MouseButton.left:
+						btn = MouseButton.Left;
+					break;
+					case arsd.simpledisplay.MouseButton.right:
+						btn = MouseButton.Right;
+					break;
+					case arsd.simpledisplay.MouseButton.middle:
+						btn = MouseButton.Middle;
+					break;
+					case arsd.simpledisplay.MouseButton.wheelUp:
+						btn = MouseButton.WheelUp;
+						screen.scrollCallbackEvent(0, -1, Clock.currTime.stdTime);
+					break;
+					case arsd.simpledisplay.MouseButton.wheelDown:
+						btn = MouseButton.WheelDown;
+						screen.scrollCallbackEvent(0, 1, Clock.currTime.stdTime);
+					break;
+					default:
+						btn = MouseButton.None;
+				}
+
+				final switch(event.type)
+				{
+					case arsd.simpledisplay.MouseEventType.buttonPressed:
+						action = MouseAction.Press;
+					break;
+					case arsd.simpledisplay.MouseEventType.buttonReleased:
+						action = MouseAction.Release;
+					break;
+					case arsd.simpledisplay.MouseEventType.motion:
+						action = MouseAction.Motion;
+						assert(screen);
+						screen.cursorPosCallbackEvent(event.x, event.y, Clock.currTime.stdTime);
+					return;
+				}
+
+				if (event.modifierState & ModifierState.leftButtonDown)
+					modifiers |= MouseButton.Left;
+				if (event.modifierState & ModifierState.rightButtonDown)
+					modifiers |= MouseButton.Right;
+				if (event.modifierState & ModifierState.middleButtonDown)
+					modifiers |= MouseButton.Middle;
+
+				// propagating button events
+				if (event.type == MouseEventType.buttonPressed  ||
+					event.type == MouseEventType.buttonReleased ||
+					event.type == MouseEventType.motion)
+				{
+					screen.mouseButtonCallbackEvent(btn, action, modifiers, Clock.currTime.stdTime);
+				}
+			},
+		);
+		flushGui(); // let OS do it's cleanup
+	}
+
+	void visibleForTheFirstTime(void delegate() dg)
+	{
+		user_dg = dg;
+	}
+
+// protected:
+	NVGContext nvg;
+	SimpleWindow simple_window;
+	Screen screen;
+	void delegate() user_dg;
+}
