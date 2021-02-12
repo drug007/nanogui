e2844f87f555849b5044db65f249421adcfadf17
drug007 <drug2004@bk.ru>
2021-02-12 14:14:39 +0300

    Initial test description

--- lixua/tests/test_description.d
+++ lixua/tests/test_description.d
@@ -2,98 +2,64 @@ module tests.test_description;
 
 import dyaml;
 
-struct Color
-{
-	import std : toLower;
-
-	ubyte red;
-	ubyte green;
-	ubyte blue;
-
-	this(ubyte r, ubyte g, ubyte b)
-	{
-		red = r;
-		green = g;
-		blue = b;
-	}
-
-	this(const Node node, string tag) @safe
-	{
-		if (tag == "!color-mapping")
-		{
-			//Will throw if a value is missing, is not an integer, or is out of range.
-			red = node["r"].as!ubyte;
-			green = node["g"].as!ubyte;
-			blue = node["b"].as!ubyte;
-		}
-		else
-		{
-			string value = node.as!string;
-
-			if(value.length != 6)
-			{
-				throw new Exception("Invalid color: " ~ value);
-			}
-			//We don't need to check for uppercase chars this way.
-			value = value.toLower();
-
-			//Get value of a hex digit.
-			uint hex(char c)
-			{
-				import std.ascii;
-				if(!std.ascii.isHexDigit(c))
-				{
-					throw new Exception("Invalid color: " ~ value);
-				}
-
-				if(std.ascii.isDigit(c))
-				{
-					return c - '0';
-				}
-				return c - 'a' + 10;
-			}
-
-			red   = cast(ubyte)(16 * hex(value[0]) + hex(value[1]));
-			green = cast(ubyte)(16 * hex(value[2]) + hex(value[3]));
-			blue  = cast(ubyte)(16 * hex(value[4]) + hex(value[5]));
-		}
-	}
-}
-
-string desc = "scalar-red: FF0000
-scalar-orange: FFFF00
-mapping-red: !color-mapping {r: 255, g: 0, b: 0}
-mapping-orange:
-    !color-mapping
-    r: 255
-    g: 255
-    b: 0";
+enum Order { forward, reverse, runtime }
 
 int main()
 {
-	import std.stdio : writeln;
+	testOrder;
+	testNestedStruct;
+	return 0;
+}
 
-	auto red    = Color(255, 0, 0);
-	auto orange = Color(255, 255, 0);
-
+auto getOrder(const(Node) node, Order default_) @safe
+{
 	try
 	{
-		auto root = Loader.fromString(desc).load();
-
-		if(root["scalar-red"].as!Color == red &&
-			root["mapping-red"].as!Color == red &&
-			root["scalar-orange"].as!Color == orange &&
-			root["mapping-orange"].as!Color == orange)
-		{
-			writeln("SUCCESS");
-			return 0;
-		}
+		import std.conv : to;
+		return node["order"].as!string.to!Order;
 	}
 	catch(YAMLException e)
 	{
-		writeln(e.msg);
+		return default_;
 	}
+}
 
-	writeln("FAILURE");
-	return 1;
+auto testOrder()
+{
+	const desc = [
+		"order: forward",
+		"order: reverse",
+		"order: runtime",
+	];
+
+	auto root = Loader.fromString(desc[0]).load();
+	assert(root.getOrder(Order.runtime) == Order.forward);
+
+	root = Loader.fromString(desc[1]).load();
+	assert(root.getOrder(Order.runtime) == Order.reverse);
+
+	root = Loader.fromString(desc[2]).load();
+	assert(root.getOrder(Order.forward) == Order.runtime);
+}
+
+auto testNestedStruct()
+{
+	const desc = "
+foo:
+    order: reverse
+    ps:
+        bar:
+            order: runtime
+";
+
+	auto root = Loader.fromString(desc).load();
+
+	auto current = root["foo"].getOrder(Order.forward);
+	assert(current == Order.reverse);
+
+	current = root["foo"]["ps"].getOrder(current);
+	assert(current == Order.reverse);
+
+	current = root["foo"]["ps"]["bar"].getOrder(current);
+	assert(current == Order.runtime);
 }
