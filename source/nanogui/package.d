91acd09814191f30da223e9a3b2d662f235f0815
drug007 <drug2004@bk.ru>
2021-04-09 12:23:02 +0300

    Add package

---
+++ source/nanogui/package.d
@@ -0,0 +1,21 @@
+module nanogui;
+
+public import
+	nanogui.button,
+	nanogui.checkbox,
+	nanogui.combobox,
+	nanogui.common,
+	nanogui.entypo,
+	nanogui.formhelper,
+	nanogui.glcanvas,
+	nanogui.label,
+	nanogui.layout,
+	nanogui.popup,
+	nanogui.popupbutton,
+	nanogui.screen,
+	nanogui.slider,
+	nanogui.textbox,
+	nanogui.theme,
+	nanogui.vscrollpanel,
+	nanogui.widget,
+	nanogui.window;
