a04e6f0170de020e6497c50e43192d10d8504ef4
drug007 <drug2004@bk.ru>
2021-05-13 17:05:19 +0300

    Refactoring

--- auxil/source/auxil/test2.d
+++ auxil/source/auxil/test2.d
@@ -19,20 +19,27 @@ struct Location
 
 struct Automata
 {
-    float total_shift = 0, shift;
+    // sum of all previous elements
+    // it can have only fixed set of values so it is called `fixed`
+    float fixedValue = 0;
+    // the value we should iterate over given sequence and
+    // can be any value
+    float destinationShift;
     Location loc;
 
     this(float v) { loc = Location(v); }
     
-    bool _complete;
+    private bool _complete;
     bool complete() { return _complete; }
     void next(int v)
     {
-        if (total_shift + v > shift)
+        if (fixedValue + v > destinationShift)
+        {
             _complete = true;
+        }
         else
         {
-            total_shift += v;
+            fixedValue += v;
             loc.next(v);
         }
     }
@@ -61,68 +68,69 @@ void testAutomata()
     float[] log;
     auto a = Automata(0);
     test(a, seq, log);
-    assert(a.total_shift == 128);
-    assert(a.total_shift == sum(seq));
+    assert(a.fixedValue == 128);
+    assert(a.fixedValue == sum(seq));
     assert(log == [0, 10, 31, 43, 56, 80, 95, 111, 128]);
 
     log = null;
     a = Automata(0);
-    a.shift = 40;
+    a.destinationShift = 40;
     test(a, seq, log);
-    assert(a.shift == 40);
-    assert(a.total_shift == 31);
-    assert(a.total_shift == sum(seq[0..2]));
+    assert(a.destinationShift == 40);
+    assert(a.fixedValue == 31);
+    assert(a.fixedValue == sum(seq[0..2]));
     assert(log == [0, 10, 31]);
 
-    // next total_shift is equal to start of an element
+    // next fixedValue is equal to start of an element
     log = null;
     a = Automata(0);
-    a.shift = 43;
+    a.destinationShift = 43;
     test(a, seq, log);
-    assert(a.total_shift == 43);
-    assert(a.shift == 43);
+    assert(a.fixedValue == 43);
+    assert(a.destinationShift == 43);
     assert(log == [0, 10, 31, 43]);
 
     log = null;
     a = Automata(0);
-    a.shift = 58;
+    a.destinationShift = 58;
     test(a, seq, log);
-    assert(a.total_shift == 56);
-    assert(a.shift == 58);
+    assert(a.fixedValue == 56);
+    assert(a.destinationShift == 58);
     assert(log == [0, 10, 31, 43, 56]);
 
     log = null;
     a = Automata(0);
     test(a, seq.retro, log);
-    assert(a.total_shift == 128);
+    assert(a.fixedValue == 128);
     const total_sum = sum(seq);
+    assert(log.equal([0, 17, 33, 48, 72, 85, 97, 118, 128]));
     assert(log.map!(a=>total_sum-a).equal([0, 10, 31, 43, 56, 80, 95, 111, 128].retro));
 
     a = Automata(0);
-    a.shift = 83;
+    a.destinationShift = 83;
     test(a, seq.retro);
-    assert(a.total_shift == 72);
-    assert(a.shift == 83);
+    assert(a.fixedValue == 72);
+    assert(a.destinationShift == 83);
     
     a = Automata(0);
-    a.shift = 85;
+    a.destinationShift = 85;
     test(a, seq.retro);
-    assert(a.total_shift == 85);
-    assert(a.shift == 85);
+    assert(a.fixedValue == 85);
+    assert(a.destinationShift == 85);
 
     // 80 70  49  37  24   0
     //   [10, 21, 12, 13, 24]
     auto subseq = seq[0..5];
 
     a = Automata(0);
-    a.shift = 85;
+    a.destinationShift = 85;
     test(a, seq[0..5].retro);
-    assert(a.total_shift == 80);
-    assert(a.shift == 85);
+    assert(a.fixedValue == 80);
+    assert(a.destinationShift == 85);
 
     a = Automata(0);
-    a.shift = 38;
+    a.destinationShift = 38;
     test(a, seq[0..5].retro);
-    assert(a.total_shift == 37);
-    assert(a.shift == 38);
+    assert(a.fixedValue == 37);
+    assert(a.destinationShift == 38);
 }
