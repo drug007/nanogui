bb1343efedb1b7dad7ce3ed5b19cb713959bc63f
drug007 <drug2004@bk.ru>
2021-05-14 13:22:46 +0300

    Refactoring

--- auxil/source/auxil/test2.d
+++ auxil/source/auxil/test2.d
@@ -28,7 +28,7 @@ struct Automata
     Location loc;
     float init_value;
 
-    this(float v) { loc = Location(v); init_value = v; }
+    this(float v) { loc = Location(0); init_value = v; }
     
     private bool _complete;
     bool complete() { return _complete; }
@@ -40,50 +40,60 @@ struct Automata
         }
         else
         {
-            fixedValue += v;
-            loc.next(v);
+            doNext(v);
         }
     }
 
-    auto position(bool forward)()
+    package void doNext(int v)
+    {
+        fixedValue += v;
+        loc.next(v);
+    }
+
+    auto position(bool forward)(float e)
     {
         static if (forward)
-            return init_value + fixedValue;
+            return init_value + loc.loc;
         else
-            return init_value - fixedValue;
+            return init_value - loc.loc - e;
     }
+}
+
+struct LogRecord
+{
+    float pos;
+    float value;
 }
 
 auto test(R)(ref Automata a, R data)
 {
-    float[] log;
+    LogRecord[] log;
     test(a, data, log);
 }
 
-auto test(bool forward, R)(ref Automata a, R r, ref float[] log)
+auto test(bool forward, R)(ref Automata a, R r, ref LogRecord[] log)
 {
-    log ~= a.loc.loc;
     static if (forward)
         auto data = r;
     else
         auto data = r.retro;
     foreach(e; data)
     {
+        log ~= LogRecord(a.position!forward(e), e);
         a.next(e);
         if (a.complete)
             break;
-        log ~= a.loc.loc;
     }
 }
 
 void testAutomata()
 {
-    float[] posLog;
+    LogRecord[] posLog;
     auto a = Automata(0);
     test!true(a, seq, posLog);
     assert(a.fixedValue == 128);
     assert(a.fixedValue == sum(seq));
-    assert(posLog == [0, 10, 31, 43, 56, 80, 95, 111, 128]);
+    assert(posLog.map!"a.pos".equal([0, 10, 31, 43, 56, 80, 95, 111]));
 
     posLog = null;
     a = Automata(0);
@@ -92,7 +102,7 @@ void testAutomata()
     assert(a.destinationShift == 40);
     assert(a.fixedValue == 31);
     assert(a.fixedValue == sum(seq[0..2]));
-    assert(posLog == [0, 10, 31]);
+    assert(posLog.map!"a.pos".equal([0, 10, 31]));
 
     // next fixedValue is equal to start of an element
     posLog = null;
@@ -101,7 +111,7 @@ void testAutomata()
     test!true(a, seq, posLog);
     assert(a.fixedValue == 43);
     assert(a.destinationShift == 43);
-    assert(posLog == [0, 10, 31, 43]);
+    assert(posLog.map!"a.pos".equal([0, 10, 31, 43]));
 
     posLog = null;
     a = Automata(0);
@@ -109,50 +119,50 @@ void testAutomata()
     test!true(a, seq, posLog);
     assert(a.fixedValue == 56);
     assert(a.destinationShift == 58);
-    assert(posLog == [0, 10, 31, 43, 56]);
+    assert(posLog.map!"a.pos".equal([0, 10, 31, 43, 56]));
 
     posLog = null;
-    a = Automata(0);
-    a.init_value = 128;
+    a = Automata(128);
     test!false(a, seq, posLog);
     assert(a.fixedValue == 128);
     const total_sum = sum(seq);
-    assert(posLog.equal([0, 17, 33, 48, 72, 85, 97, 118, 128]));
-    assert(posLog.map!(a=>total_sum-a).equal([0, 10, 31, 43, 56, 80, 95, 111, 128].retro));
+
+    assert(posLog.map!"a.pos".equal([111, 95, 80, 56, 43, 31, 10, 0]));
+    assert(posLog.map!"a.pos".equal([0, 10, 31, 43, 56, 80, 95, 111].retro));
 
     posLog = null;
-    a = Automata(0);
+    a = Automata(128);
     a.destinationShift = 83;
     test!false(a, seq, posLog);
     assert(a.fixedValue == 72);
     assert(a.destinationShift == 83);
-    assert(posLog.equal([0, 17, 33, 48, 72]));
+    assert(posLog.map!"a.pos".equal([111, 95, 80, 56, 43]));
     
     posLog = null;
-    a = Automata(0);
+    a = Automata(128);
     a.destinationShift = 85;
     test!false(a, seq, posLog);
     assert(a.fixedValue == 85);
     assert(a.destinationShift == 85);
-    assert(posLog.equal([0, 17, 33, 48, 72, 85]));
+    assert(posLog.map!"a.pos".equal([111, 95, 80, 56, 43, 31]));
 
     // 80 70  49  37  24   0
     //   [10, 21, 12, 13, 24]
     auto subseq = seq[0..5];
 
     posLog = null;
-    a = Automata(0);
+    a = Automata(sum(subseq));
     a.destinationShift = 85;
-    test!false(a, seq[0..5], posLog);
+    test!false(a, subseq, posLog);
     assert(a.fixedValue == 80);
     assert(a.destinationShift == 85);
-    assert(posLog.equal([0, 24, 37, 49, 70, 80]));
+    assert(posLog.map!"a.pos".equal([56, 43, 31, 10, 0]));
 
     posLog = null;
-    a = Automata(0);
+    a = Automata(sum(subseq));
     a.destinationShift = 38;
-    test!false(a, seq[0..5], posLog);
+    test!false(a, subseq, posLog);
     assert(a.fixedValue == 37);
     assert(a.destinationShift == 38);
-    assert(posLog.equal([0, 24, 37]));
+    assert(posLog.map!"a.pos".equal([56, 43, 31]));
 }
