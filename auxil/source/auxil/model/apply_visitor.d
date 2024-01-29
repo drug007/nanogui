632978e16efa9fc9c09ed1412dca31a07ab42fa2
drug007 <drug2004@bk.ru>
2024-01-29 23:25:38 +0300

    Added apply_visitor module

---
+++ auxil/source/auxil/model/apply_visitor.d
@@ -0,0 +1,53 @@
+module auxil.model.apply_visitor;
+
+import auxil.model : TreePathVisitor, Order, traversalForward;
+
+void applyByTreePath(T, Data, Model)(auto ref Data data, ref Model model, const(int)[] path, void delegate(ref const(T) value) dg)
+{
+	auto pv = ApplyVisitor!T();
+	pv.path.value = path;
+	pv.dg = dg;
+	model.traversalForward(data, pv);
+}
+
+private struct ApplyVisitor(T)
+{
+	import std.typecons : Nullable;
+
+	TreePathVisitor default_visitor;
+	alias default_visitor this;
+
+	void delegate(ref const(T) value) dg;
+	bool completed;
+
+	bool complete()
+	{
+		return completed;
+	}
+
+	void enterNode(Order order, Data, Model)(ref const(Data) data, ref Model model)
+	{
+		static if (is(Data == T))
+		{
+			completed = tree_path.value[] == path.value[];
+			if (completed)
+			{
+				dg(data);
+				return;
+			}
+		}
+
+		processLeaf!order(data, model);
+	}
+
+	void processLeaf(Order order, Data, Model)(ref const(Data) data, ref Model model)
+	{
+		static if (is(Data == T))
+		{
+			assert(!completed);
+			completed = tree_path.value[] == path.value[];
+			if (completed)
+				dg(data);
+		}
+	}
+}
--- auxil/source/auxil/model/package.d
+++ auxil/source/auxil/model/package.d
@@ -721,56 +721,6 @@ private auto getLength(Data, alias data)()
 		static assert(0);
 }
 
-void applyByTreePath(T, Data, Model)(auto ref Data data, ref Model model, const(int)[] path, void delegate(ref const(T) value) dg)
-{
-	auto pv = ApplyVisitor!T();
-	pv.path.value = path;
-	pv.dg = dg;
-	model.traversalForward(data, pv);
-}
-
-private struct ApplyVisitor(T)
-{
-	import std.typecons : Nullable;
-
-	TreePathVisitor default_visitor;
-	alias default_visitor this;
-
-	void delegate(ref const(T) value) dg;
-	bool completed;
-
-	bool complete()
-	{
-		return completed;
-	}
-
-	void enterNode(Order order, Data, Model)(ref const(Data) data, ref Model model)
-	{
-		static if (is(Data == T))
-		{
-			completed = tree_path.value[] == path.value[];
-			if (completed)
-			{
-				dg(data);
-				return;
-			}
-		}
-
-		processLeaf!order(data, model);
-	}
-
-	void processLeaf(Order order, Data, Model)(ref const(Data) data, ref Model model)
-	{
-		static if (is(Data == T))
-		{
-			assert(!completed);
-			completed = tree_path.value[] == path.value[];
-			if (completed)
-				dg(data);
-		}
-	}
-}
-
 enum Order { Sinking, Bubbling, }
 
 private struct TwoFacedRange(Order order)
