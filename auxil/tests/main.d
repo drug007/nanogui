b0f845a27fb7a34dc98fb357d56d704b70cab62e
drug007 <drug2004@bk.ru>
2024-01-30 00:50:16 +0300

    Fix tests

--- auxil/tests/main.d
+++ auxil/tests/main.d
@@ -4,4 +4,13 @@ mixin runTestsMain!(
 	"auxil.model",
 	"auxil.traits",
 	"auxil.test",
+	"auxil.apply_visitor",
+	"auxil.common",
+	"auxil.default_visitor",
+	"auxil.fixedappender",
+	"auxil.property_visitor",
+	"auxil.test",
+	"auxil.traits",
+	"auxil.tree_path",
+	"auxil.two_faced_range",
 );
