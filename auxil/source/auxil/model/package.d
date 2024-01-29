2cdfc5aeb94a339378f42d731f23d3f7ef5b5c60
drug007 <drug2004@bk.ru>
2024-01-29 23:31:18 +0300

    Added tree_path module

--- auxil/source/auxil/model/package.d
+++ auxil/source/auxil/model/package.d
@@ -758,56 +758,6 @@ void traversalBackward(Model, Data, Visitor)(ref Model model, auto ref Data data
 	model.accept!order(data, visitor);
 }
 
-struct TreePath
-{
-	import std.experimental.allocator.mallocator : Mallocator;
-	import automem.vector : Vector;
-
-@safe:
-
-	Vector!(int, Mallocator)  value;
-
-	ref int back() return @nogc
-	{
-		assert(value.length);
-		return value[$-1];
-	}
-
-	void popBack() @nogc
-	{
-		value.popBack;
-	}
-
-	void clear() @nogc
-	{
-		value.clear;
-	}
-
-	auto put(int i) @nogc @trusted
-	{
-		value.put(i);
-	}
-
-	import std.range : isOutputRange;
-	import std.format : FormatSpec;
-
-	void toString(Writer) (ref Writer w, scope const ref FormatSpec!char fmt) const  @trusted
-		if (isOutputRange!(Writer, char))
-	{
-		import std;
-		import std.conv : text;
-
-		w.put('[');
-		if (value.length)
-		{
-			foreach(e; value[0..$-1])
-				copy(text(e, "."), w);
-			copy(text(value[$-1]), w);
-		}
-		w.put(']');
-	}
-}
-
 version(unittest) @Name("null_visitor")
 unittest
 {
@@ -849,6 +799,8 @@ struct DefaultVisitorImpl(
 
 	static if (treePathEnabled == TreePathEnabled.yes)
 	{
+		import auxil.model.tree_path : TreePath;
+
 		enum State { seeking, first, rest, finishing, }
 		State state;
 		TreePath tree_path, path;
---
+++ auxil/source/auxil/model/tree_path.d
@@ -0,0 +1,51 @@
+module auxil.model.tree_path;
+
+struct TreePath
+{
+	import std.experimental.allocator.mallocator : Mallocator;
+	import automem.vector : Vector;
+
+@safe:
+
+	Vector!(int, Mallocator)  value;
+
+	ref int back() return @nogc
+	{
+		assert(value.length);
+		return value[$-1];
+	}
+
+	void popBack() @nogc
+	{
+		value.popBack;
+	}
+
+	void clear() @nogc
+	{
+		value.clear;
+	}
+
+	auto put(int i) @nogc @trusted
+	{
+		value.put(i);
+	}
+
+	import std.range : isOutputRange;
+	import std.format : FormatSpec;
+
+	void toString(Writer) (ref Writer w, scope const ref FormatSpec!char fmt) const  @trusted
+		if (isOutputRange!(Writer, char))
+	{
+		import std;
+		import std.conv : text;
+
+		w.put('[');
+		if (value.length)
+		{
+			foreach(e; value[0..$-1])
+				copy(text(e, "."), w);
+			copy(text(value[$-1]), w);
+		}
+		w.put(']');
+	}
+}
