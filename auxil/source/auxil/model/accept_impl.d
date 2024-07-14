2acd56d1d3e11c61d9c72286d9b5a0665fc4e853
drug007 <drug2004@bk.ru>
2024-07-14 16:38:34 +0300

    Empty ranges is not displayed
    should be a tunable parameter

--- auxil/source/auxil/model/accept_impl.d
+++ auxil/source/auxil/model/accept_impl.d
@@ -27,6 +27,16 @@ mixin template acceptImpl()
 		enum hasTreePath = Visitor.treePathEnabled;
 		enum hasSize     = Visitor.sizeCalculationEnabled;
 
+		// If the range is empty then it is not processed
+		// TODO: should be a tunable parameter per a node(!)
+		static if (dataHasStaticArrayModel!Data || 
+		           dataHasRandomAccessRangeModel!Data ||
+		           dataHasAssociativeArrayModel!Data)
+		{
+			if (!data.length)
+				return false;
+		}
+
 		if (visitor.doEnterNode!(order, Data)(data, this, visitor))
 			return true;
 
